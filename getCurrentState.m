1ba9a651938b47185f2f8db700e04d8641e733c4
mkrumin <mkrumin@gmail.com>
2017-08-19 21:15:38 +0100

    unfinished debugging of the loading bug

--- getCurrentState.m
+++ getCurrentState.m
@@ -2,9 +2,21 @@ function state = getCurrentState(h)
 
 state.CurrentFolder = h.CurrentFolder;
 state.FileName = h.FileName;
+state.iFrame = h.iFrame;
 state.roi = h.roi;
 state.blinkRoi = h.blinkRoi;
 state.blinkClassifier = h.blinkClassifier;
 state.Threshold = h.ThresholdSlider.Value;
 state.FilterSize = h.FilterSizeEdit.Value;
+state.cMax = h.MaxSlider.Value;
+state.cMin = h.MinSlider.Value;
+state.showOriginal =
+state.showFiltered =
+state.showBW = 
+state.showCenter
+state.showEdge = 
+state.showEllipse = 
+state.showROI = 
+state.showCropped = 
+
 state.analyzedFrames = h.analyzedFrames;

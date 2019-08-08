3e23661a1e324643505dd6176acc80ad4705ddb7
Michael Krumin <mkrumin@gmail.com>
2019-08-08 17:58:26 +0100

    Introduced multicore reading of video files

--- loadPars.m
+++ loadPars.m
@@ -2,5 +2,12 @@ function pars = loadPars()
 
 p = dat.paths;
 pars.initFolder = fullfile(p.mainRepository, 'PC041');
-pars.localTempFileName = 'R:\Temp\tmp';
-pars.batchSize = 2000;
+pars.localTempFileName = 'R:\Temp\tmp'; % it is safer to first copy the file locally, and then open it
+% it is also faster if the local copy is on fast SSD, or even on virtual RAM drive
+pars.batchSize = 2048;
+% Because the videos are compressed it takes a lot of CPU power to load the
+% frames, and it becomes a bottleneck. This can be improved if loading
+% frames on independent cores, but must not be used on HDDs or when reading
+% directly from the server
+pars.useMulticoreForReading = true; % only use with local temp copy, and only on SSD/RAM
+pars.framesPerCore2Read = 64; % used if useMulticoreForReading == true
--- readBatch.m
+++ readBatch.m
@@ -1,16 +1,49 @@
-function frames = readBatch(vr, idx)
+function frames = readBatch(vr, indices)
 
-idx = sort(idx); %just in case
-frames = zeros(vr.Height, vr.Width, length(idx), 'uint8');
+indices = sort(indices); %just in case
+nFrames = length(indices);
+% frames = zeros(vr.Height, vr.Width, nFrames, 'uint8');
 % find blocks of consecutive frames
 
-% delta will be constant for consecutive frames, and will jump otherwise
-delta = idx(:) - [1:length(idx)]';
-blockIDs = unique(delta);
-nBlocks = length(blockIDs);
+pars = loadPars;
 
-for iBlock = 1:nBlocks
-    frameIdx = find(delta==blockIDs(iBlock));
-    tmp = read(vr, [idx(frameIdx(1)), idx(frameIdx(end))]);
-    frames(:,:,frameIdx) = squeeze(tmp);
+% Because the videos are compressed it takes a lot of CPU power to load the
+% frames, and it becomes a bottleneck. This can be improved if loading
+% frames on independent cores, but must not be used on HDDs or when reading
+% directly from the server
+useMulticore = pars.useMulticoreForReading;
+
+if useMulticore
+    partSize = pars.framesPerCore2Read;
+    nParts = ceil(nFrames/partSize);
+    startInd = round(linspace(1, nFrames+1, nParts+1));
+    for iPart = 1:nParts
+        ind2read{iPart} = indices(startInd(iPart):(startInd(iPart+1)-1));
+    end
+    parfor iPart = 1:nParts
+        tmp{iPart} = ...
+            readFrames(vr, ind2read{iPart});
+    end
+    frames = cell2mat(reshape(tmp, 1, 1, nParts));
+else
+    frames = readFrames(vr, indices);
+end
+
+end
+
+function data = readFrames(vr, idx)
+    
+    data = zeros(vr.Height, vr.Width, length(idx), 'uint8');
+
+    % delta will be constant for consecutive frames, and will jump otherwise
+    delta = idx(:) - [1:length(idx)]';
+    blockIDs = unique(delta);
+    nBlocks = length(blockIDs);
+    
+    for iBlock = 1:nBlocks
+        frameIdx = find(delta==blockIDs(iBlock));
+        tmp = read(vr, [idx(frameIdx(1)), idx(frameIdx(end))]);
+        data(:,:,frameIdx) = squeeze(tmp);
+    end
+
 end
